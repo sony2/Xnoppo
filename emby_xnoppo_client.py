97a71b643b2cb3c5a8f7424b788f04ca8e371518
oscalarr <oscar.larrayoz@helvetia.es>
2022-07-17 08:17:51 +0200

    Simplificación del codigo del servidor web

--- emby_xnoppo_client.py
+++ emby_xnoppo_client.py
@@ -1,18 +1,18 @@
 import sys
 import os
 
-print("argv was",sys.argv)
+print("argv was", sys.argv)
 print("sys.executable was", sys.executable)
-       
+
 cwd = os.path.dirname(os.path.abspath(__file__))
 if sys.platform.startswith('win'):
-   separador="\\"
+    separador = "\\"
 else:
-   separador="/"
-cfile=cwd + separador + "xnoppo_web.py"
+    separador = "/"
+cfile = cwd + separador + "xnoppo_web.py"
 
 while True:
     command = sys.executable + ' "' + cfile + '"'
     print(command)
-    #print("restart now")
+    # print("restart now")
     os.system(command)
